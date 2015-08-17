7b7aae009bcba951f2a1376bd0292362268ffbb2
Svätopluk Bob Hanzel <svatopluk@bobcode.eu>
2015-08-17 14:20:44 +0200

    Added API

---
+++ application/controllers/api.php
@@ -0,0 +1,13 @@
+<?php
+	class Api extends CI_Controller {
+		public function bufet(){
+			$this->load->helper('operations');
+
+			return $this->output
+	            ->set_content_type('application/json')
+	            ->set_output(json_encode(array(
+	                    'multiplier' => operations_ledcoin_multiplier()
+	            )));
+		}
+	}
+?>
