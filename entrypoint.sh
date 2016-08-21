be3229acf9457e67a06d6ca06cc882e0ae897e69
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2016-08-21 15:22:54 +0200

    Updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+echo Configuring app ...
+
 cd /var/www/html
 rm -f config.yaml
 
 echo "env: '${APP_ENV}'" >> config.yaml
-echo "host: '${APP_ENV}'" >> config.yaml
+echo "host: '${APP_HOST}'" >> config.yaml
 echo "db:" >> config.yaml
 echo "  hostname: '${DB_HOSTNAME}'" >> config.yaml
 echo "  username: '${DB_USERNAME}'" >> config.yaml
 echo "  password: '${DB_PASSWORD}'" >> config.yaml
 echo "  database: '${DB_DATABASE}'" >> config.yaml
 
-cat config.yaml
+php index.php cli config_from_yaml
+php index.php cli migration 0
 
 echo Starting apache2-foreground ...
 apache2-foreground
